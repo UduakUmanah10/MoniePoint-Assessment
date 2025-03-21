6d74f75d574a2d1e005a6134f9e3b60e1c2d9195
uduak <uduakumanah10@gmail.com>
2025-03-21 01:03:40 +0000

    ThemeSetup

---
+++ app/src/main/java/com/safetyapp/moniepointassessment/ui/theme/ShippingAppFont.kt
@@ -0,0 +1,14 @@
+package com.safetyapp.moniepointassessment.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.safetyapp.moniepointassessment.R
+
+val OpenSans = FontFamily(
+    Font(R.font.roboto_bold, FontWeight.Normal),
+    Font(R.font.opensans_bold, FontWeight.Normal),
+    Font(R.font.opensans_regular, FontWeight.SemiBold),
+    Font(R.font.opensans_semibold, FontWeight.Bold),
+    Font(R.font.roboto, FontWeight.Normal),
+)
